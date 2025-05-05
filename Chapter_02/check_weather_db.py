014f78cad221f3d97fa3bbfab5e5dc0859e72eb4
HanyE <hany.elgaml@zenapptic.ai>
2025-05-04 21:39:17 -0400

    Run botpress in docker-compose

--- Chapter_02/check_weather_db.py
+++ Chapter_02/check_weather_db.py
@@ -1,3 +1,6 @@
+# Before you run this script make sure that the weather.db exists
+# if it does not exist run the following script:
+# python create_sqlite_database.py
 import sqlite3
 conn = sqlite3.connect("weather.db")
 cursor = conn.cursor()
